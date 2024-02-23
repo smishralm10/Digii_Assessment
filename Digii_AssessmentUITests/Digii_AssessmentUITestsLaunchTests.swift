f9f95108357fffcf05eba04ff5241076a14aebb0
Shreyansh Mishra <32999485+smishralm10@users.noreply.github.com>
2024-02-23 21:52:33 +0530

    Initial Commit

---
+++ Digii_AssessmentUITests/Digii_AssessmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Digii_AssessmentUITestsLaunchTests.swift
+//  Digii_AssessmentUITests
+//
+//  Created by Shreyansh Mishra on 23/02/24.
+//
+
+import XCTest
+
+final class Digii_AssessmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
